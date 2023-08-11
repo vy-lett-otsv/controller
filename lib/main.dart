ee4000be67499540a4fe505f2ef1484aa341aaa6
vy-lett-otsv <vy.lett@one-line.com>
2023-08-11 10:54:58 +0700

    change text controller to Sahami

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Controller",
+      title: "Sahami",
       debugShowCheckedModeBanner: false,
       onGenerateRoute: NavigationServices.instance.routeBuilders,
     );
