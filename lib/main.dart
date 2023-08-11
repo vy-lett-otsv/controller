73a6dae269dfc14886a2b9858159457ee72530f0
vy-lett-otsv <vy.lett@one-line.com>
2023-08-11 10:53:50 +0700

    change title in main

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Sahami",
+      title: "Controller",
       debugShowCheckedModeBanner: false,
       onGenerateRoute: NavigationServices.instance.routeBuilders,
     );
